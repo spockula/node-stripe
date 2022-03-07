49242adb39bc244498f53e0b9d6a8dcc2fdfb174
spockula <chinedukogu@gmail.com>
2022-03-07 11:22:46 +0100

    updated params

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-// var cors = require('cors');
+const axios = require('axios');
 const stripe = require('stripe')('sk_test_51KP6OgCU7wHmOgIeOzCGrwvAHsBA5J3LxUO3De4D24UwBw2ZmfbWufRL9mOGI5bXpqmRvBKI7rJmuRCuROgsGbhV000lpLt332');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}) );
@@ -14,24 +14,24 @@ app.all("/*", function(req, res, next){
 });
 
 app.post('/charge', async (req, res) => {
-        stripe.paymentIntents.create(
-            {
-              amount: parseInt(req.body.amount),
-              currency: "usd",
-              payment_method_types: ["card"],
-              receipt_email: "chinedukogu@gmail.com",
-            },
-            function (err, paymentIntent) {
-              if (err) {
-                res.status(500).json(err.message);
-              } else {
-                res.status(201).json(paymentIntent);
-                console.log('intent', paymentIntent)
-              }
-            }
-          );
-
+    stripe.paymentIntents.create(
+        {
+          amount: parseInt(req.body.amount),
+          currency: "usd",
+          payment_method_types: ["card"],
+          receipt_email: req.body.email,
+        },
+        function (err, paymentIntent) {
+          if (err) {
+            res.status(500).json(err.message);
+          } else {
+            res.status(201).json(paymentIntent);
+          }
+        }
+      );
 });
+
+  
 
 const port = process.env.PORT || 3000;
 app.listen(process.env.PORT || 3000);
